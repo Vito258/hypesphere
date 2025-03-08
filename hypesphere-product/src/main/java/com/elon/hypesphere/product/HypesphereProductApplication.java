5a0700a71ae03603d6daaf8b39380bb97be5c05d
Vito258 <3090095224@qq.com>
2025-03-08 11:50:12 +0800

    配置MybatisPlus 代码生成器

--- hypesphere-product/src/main/java/com/elon/hypesphere/product/HypesphereProductApplication.java
+++ hypesphere-product/src/main/java/com/elon/hypesphere/product/HypesphereProductApplication.java
@@ -1,11 +1,11 @@
 package com.elon.hypesphere.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HypesphereProductApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HypesphereProductApplication.class, args);
 	}
